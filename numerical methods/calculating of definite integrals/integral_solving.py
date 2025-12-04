ff4d1b7e4e586e01c9a1c5905aba2079b1143c7c
blen36 <bankoegor@gmail.com>
2025-12-04 20:19:17 +0300

    change logic in simpson's method

--- numerical methods/calculating of definite integrals/integral_solving.py
+++ numerical methods/calculating of definite integrals/integral_solving.py
@@ -7,14 +7,12 @@ def trapezoid(a, b, eps):
     n = 4
     h = (b - a) / n
 
-    # Первое вычисление
     I_prev = h * (0.5*f(a) + 0.5*f(b) + sum(f(a + k*h) for k in range(1, n)))
 
     while True:
         n *= 2
         h /= 2
 
-        # Полный пересчёт по формуле трапеций
         I_new = h * (0.5*f(a) + 0.5*f(b) + sum(f(a + k*h) for k in range(1, n)))
 
         if abs(I_new - I_prev) < eps:
@@ -22,18 +20,13 @@ def trapezoid(a, b, eps):
 
         I_prev = I_new
 
+
 def simpson(a, b, eps):
-    n = 4       # n должно оставаться чётным
+    n = 2
     h = (b - a) / n
 
-    # Функция полного пересчёта по формуле Симпсона
-    def Simpson_full(n, h):
-        S_odd  = sum(f(a + k*h) for k in range(1, n, 2))
-        S_even = sum(f(a + k*h) for k in range(2, n, 2))
-        return (h/3) * (f(a) + f(b) + 4*S_odd + 2*S_even)
-
-    # Первое вычисление
-    I_prev = Simpson_full(n, h)
+    # Первое вычисление (n=2)
+    I_prev = (h / 3) * (f(a) + 4 * f(a + h) + f(b))
 
     while True:
         n *= 2
@@ -41,10 +34,23 @@ def simpson(a, b, eps):
 
         I_new = Simpson_full(n, h)
 
-        if abs(I_new - I_prev) < eps:
+        if abs(I_new - I_prev) < 15 * eps:
             return I_new, n
 
         I_prev = I_new
+
+def Simpson_full(n, h):
+    S_odd = 0
+    S_even = 0
+    # нечет
+    for k in range(1, n, 2):
+        S_odd += f(a + k * h)
+
+    # чет
+    for k in range(2, n, 2):
+        S_even += f(a + k * h)
+
+    return (h / 3) * (f(a) + f(b) + 4 * S_odd + 2 * S_even)
 
 if __name__ == "__main__":
     a = 0.8
@@ -61,18 +67,15 @@ if __name__ == "__main__":
         print(f"Метод трапеций:   I = {I_trap:.10f}, узлов = {N_trap}")
         print(f"Метод Симпсона:   I = {I_simp:.10f}, узлов = {N_simp}")
 
-        # Сравнение результатов
         diff = abs(I_simp - I_trap)
-        rel_diff = diff / abs(I_simp) if I_simp != 0 else 0
 
         print("\n--- Сравнение методов ---")
         print(f"Абсолютная разница:      {diff:.10e}")
-        print(f"Относительная разница:   {rel_diff:.10e}")
 
         if N_simp < N_trap:
-            print("Метод Симпсона оказался БЫСТРЕЕ (меньше узлов).")
+            print("Метод Симпсона оказался быстрее (меньше узлов).")
         elif N_simp > N_trap:
-            print("Метод трапеций оказался быстрее — редкий случай!")
+            print("Метод трапеций оказался быстрее")
         else:
             print("Оба метода использовали одинаковое количество узлов.")
 
